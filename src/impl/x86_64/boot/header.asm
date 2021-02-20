df4d47e4e188ea5958c1b2c947b98fc8352b85d6
tontsa28 <xdman7917@gmail.com>
2021-02-20 20:11:01 +0200

    added Dockerfile and header.asm

---
+++ src/impl/x86_64/boot/header.asm
@@ -0,0 +1,10 @@
+header_start:
+    ; magic number
+    dd 0xe85250d6
+    ; architecture
+    dd 0 ; protected mode i386
+    ; header length
+    dd header_end - header_start
+    ; checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+header_end:
