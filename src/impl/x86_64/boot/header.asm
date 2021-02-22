e3b2613958ee5ffb14378451125b805484436135
tontsa28 <xdman7917@gmail.com>
2021-02-22 14:25:50 +0200

    added several system kernel files
    added Makefile, edited header.asm, added main.asm, added .gitignore, added grub.cfg and added linker.ld

--- src/impl/x86_64/boot/header.asm
+++ src/impl/x86_64/boot/header.asm
@@ -1,3 +1,4 @@
+section .multiboot_header
 header_start:
     ; magic number
     dd 0xe85250d6
@@ -7,4 +8,8 @@ header_start:
     dd header_end - header_start
     ; checksum
     dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+    ; end tag
+    dw 0
+    dw 0
+    dd 8
 header_end:
