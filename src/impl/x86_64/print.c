917f37b3fc216860f3a5ef40bab8bb8b56dfbe29
tontsa28 <xdman7917@gmail.com>
2021-08-26 11:15:20 +0300

    Forgot a goddamn semicolon...

--- src/impl/x86_64/print.c
+++ src/impl/x86_64/print.c
@@ -17,7 +17,7 @@ void clear_row(size_t row) {
 
     struct Char empty = (struct Char) {
         character: ' ',
-        color: color,
+        color: color
     };
 
     for (size_t col = 0; col < NUM_COLS; col++) {
