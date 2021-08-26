5f87db0fd3efc97734d240f2fe1127f8606c2519
tontsa28 <xdman7917@gmail.com>
2021-08-26 11:21:23 +0300

    Added line changing after the first printed line

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -4,7 +4,7 @@ void kernel_main() {
 
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Welcome to this 64-bit kernel!");
+    print_str("Welcome to this 64-bit kernel!\n");
     print_str("This is version 1.0 of the kernel!");
     
 }
